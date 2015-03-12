044f2cebe05d8f73646ee74c82ad775028f7c9e7
Abhishek Pareek <makeittotop@gmail.com>
2015-03-12 21:18:26 +0400

    Cleaned up js and added call to perform an rpc ajax call on blur event. Version 3.

--- popup.js
+++ popup.js
@@ -1,25 +1,34 @@
-$(document).ready(function() {   
+var get_currency_rate = function () {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "http://www.xe.com/currencyconverter/convert/?Amount=1&From=AED&To=INR", true);
+
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+      var el = document.createElement( 'div' );
+      el.innerHTML = xhr.responseText;
+
+      var currency_text;
+      $('.uccRes .rightCol', el).each(function() { currency_text = $(this).html();});
+
+      var re = /\d+.\d+/;
+      var current_rate = currency_text.match(re)[0];
+      var aed_amount = $("#aed_amount").val()
+      $("#inr_amount").val(aed_amount * current_rate);
+    }
+  }
+
+  xhr.send();
+};
+
+$(document).ready(function() {
+  get_currency_rate();
 
   $("#get_rate").click(function() {
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "http://www.xe.com/currencyconverter/convert/?Amount=1&From=AED&To=INR", true);
+    get_currency_rate();
+  });
 
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState == 4) {
-        var el = document.createElement( 'div' );
-        el.innerHTML = xhr.responseText;
-
-        var currency_text;
-        $('.uccRes .rightCol', el).each(function() { currency_text = $(this).html();});
-
-        var re = /\d+.\d+/;
-        var current_rate = currency_text.match(re)[0];
-        var aed_amount = $("#aed_amount").val()
-        $("#inr_amount").val(aed_amount * current_rate)
-      }
-    }    
-
-    xhr.send();
+  $("#aed_amount").blur(function() {
+    get_currency_rate();
   });
 
 });
